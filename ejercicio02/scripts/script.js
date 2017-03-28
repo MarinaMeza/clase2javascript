a4d1beab40c139355cc6ea6343bb6d0c48460c83
alumno <alumno>
2017-03-28 20:21:40 -0300

    ejercicio 02 segundo commit

--- ejercicio02/scripts/script.js
+++ ejercicio02/scripts/script.js
@@ -1,4 +1,13 @@
-function sumar()
+function Sumar()
 {
-    
+    var numero1;
+    var numero2;
+    var resultado;
+    var txtresultado;
+
+    numero1=parseInt(document.getElementById("numero1").value);
+    numero2=parseInt(document.getElementById("numero2").value);
+    txtresultado=document.getElementById("resultado");
+    resultado=numero1+numero2;
+    txtresultado.value=resultado;
 }
